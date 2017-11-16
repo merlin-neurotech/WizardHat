b33f422793030e9915bf0b0de285a825381b720d
Matt L. Laporte <Matt.L.Laporte@gmail.com>
2017-11-16 16:23:40 -0500

    Fixed variable references.

--- muse-monitor/monitor.py
+++ muse-monitor/monitor.py
@@ -10,7 +10,7 @@ import gui
 
 if __name__ == '__main__':
     streamer = online.LSLStreamer(online.get_lsl_inlet())
-    gui.init_eegplot()
+    #gui.init_eegplot()
 
     #streamer.start() # start EEG streamer thread
     #streamer.join() # wait for EEG streamer to stop streaming
