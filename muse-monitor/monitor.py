27c96c0022ef2009066d8b315709bb7599b04b33
Matt Laporte <matt.l.laporte@gmail.com>
2017-11-16 11:08:07 -0500

    Project structure.

---
+++ muse-monitor/monitor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+"""
+import numpy as np
+
+import online
+import gui
+
+
+if __name__ == '__main__':
+    streamer = online.LSLStreamer(online.get_lsl_inlet())
+    gui.init_eegplot()
+
+    #streamer.start() # start EEG streamer thread
+    #streamer.join() # wait for EEG streamer to stop streaming
