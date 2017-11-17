fbb953e3500f4ef027b7bf18d54543098479bc17
Matt L. Laporte <Matt.L.Laporte@gmail.com>
2017-11-17 14:20:53 -0500

    Move operations to methods.

--- muse-monitor/online.py
+++ muse-monitor/online.py
@@ -2,8 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Management of data streams or other online processes.
-
-Note that the module `threading` does not
 """
 
 import utils
@@ -11,17 +9,21 @@ import utils
 from serial.serialutil import SerialException
 import threading
 
+import numpy as np
 import pylsl as lsl
 
 
 class LSLStreamer(threading.Thread):
 
-    def __init__(self, inlet, data_buffer=None):
+    def __init__(self, inlet, data_buffer=None, dejitter=True):
         threading.Thread.__init__(self)
         if data_buffer is None:
             data_buffer = LSLDataBuffer(inlet)
         self.data_buffer = data_buffer
         self.inlet = inlet
+        self.dejitter = dejitter
+        self.sfreq = self.data_buffer.sfreq
+        self.n_samples = self.data_buffer.n_samples
 
     def run(self):
         try:
@@ -29,31 +31,58 @@ class LSLStreamer(threading.Thread):
                 samples, timestamps = self.inlet.pull_chunk(timeout=1.0,
                                                             max_samples=12)
                 if timestamps:
-                    timestamps = np.arange(len(timestamps), dtype=np.float64)
-                    timestamps /= sfreq
-                    timestamps += times[-1] + 1./sfreq
-                    self.times = np.concatenate([self.times, timestamps])
-                    self.times = self.times[-n_samples:]
-                    self.data = np.vstack([self.data, samples])
-                    self.data = self.data[-n_samples:]
-                    with data_buffer.lock:
+                    if self.dejitter:
+                        timestamps = self.dejitter(timestamps, self.last_time)
+                    self.update_data(timestamps, samples)
+                    with self.data_buffer.lock:
                         self.data_buffer.times = np.copy(self.times)
-                        self.data_buffer.data = np.copy(self.data)
-        except SerialException:
-            print("EEG streaming interrupted. Device disconnected?")
+                        self.data_buffer.data = np.copy(self.samples)
 
+        except SerialException:
+            print("BGAPI streaming interrupted. Device disconnected?")
+
+    def dejitter_timestamps(self, timestamps, last_time):
+        dejittered = np.arange(len(timestamps), dtype=np.float64)
+        dejittered /= self.sfreq
+        dejittered += last_time + 1./self.sfreq
+        return dejittered
+
+    def updated_data(self, timestamps, samples):
+        times = np.concatenate([self.times, timestamps])
+        times = self.times[-self.n_samples:]
+        samples = np.vstack([self.samples, samples])
+        samples = self.samples[-self.n_samples:]
+        return times, samples
+
+    @property
+    def last_time(self):
+        return self.data_buffer.times[-1]
 
 class LSLDataBuffer(object):
 
     def __init__(self, inlet, window=5):
         self.inlet = inlet
         self.window = window
-        sfreq = inlet.info.nominal_srate()
-        n_chan = inlet.info.channel_count()
-        n_samples = int(window * sfreq)
-        self.data = np.zeros((n_samples, n_chan))
-        self.times = np.arange(-window, 0, 1./sfreq)
+
+        info = inlet.info()
+        self.sfreq = info.nominal_srate()
+        self.n_chan = info.channel_count()
+        self.n_samples = int(window * self.sfreq)
+        self.ch_names = list(self.__ch_names(info))
+
+        self.zero_data()
+
         self.lock = threading.Lock()
+
+    def zero_data(self):
+        self.samples = np.zeros((self.n_samples, self.n_chan))
+        self.times = np.arange(-self.window, 0, 1./self.sfreq)
+
+    def __ch_names(self, info):
+        ch_xml = info.desc().child('channels').first_child()
+        for ch in range(self.n_chan):
+            yield ch_xml.child_value('label')
+            ch_xml = ch_xml.next_sibling()
 
 
 def get_lsl_inlet(stream_type='EEG'):
--- muse-monitor/vis.py
+++ muse-monitor/vis.py
@@ -12,30 +12,41 @@ import seaborn as sns
 
 class LSLBufferPlot(threading.Thread):
 
-    def __init__(self, data_buffer, subsample=2, figsize=(15, 6)):
+    def __init__(self, data_buffer, subsample=2, figsize=(15, 6),
+                 plot_params=None):
         self.data_buffer = data_buffer
-        sns.set(style="whitegrid")
+        self.subsample = subsample
+        if plot_params is None:
+            plot_params = dict(style='whitegrid')
+        n_chan = data_buffer.n_chan
+
+        sns.set(**plot_params)
         fig, axes = plt.subplots(1, 1, figsize=figsize, sharex=True)
         axes.set_xlabel('Time (s)')
         axes.xaxis.grid(False)
-        axes.set_ylim(-data_buffer.n_chan + 0.5, 0.5)
-        ticks = np.arange(0, -data_buffer.n_chan, -1)
+        axes.set_ylim(-n_chan + 0.5, 0.5)
+        ticks = np.arange(0, -n_chan, -1)
         axes.set_yticks(ticks)
         impedances = np.std(data_buffer.data, axis=0)
-        ticks_labels = ['{} - {}'.format(data_buffer.ch_names[ch], impedances[ch])
-                        for ch in range(data_buffer.n_chan)]
-        axes.set_yticklabels(ticks_labels)
+        line_labels = [format_line_label(ch) for ch in range(n_chan)]
+        axes.set_yticklabels(line_labels)
         sns.despine(left=True)
+        self.fig, self.axes = fig, axes
 
-        lines = []
-        for ch in range(data_buffer.n_chan):
-            line, = axes.plot(data_buffer.times[::subsample],
-                              data_buffer.data[::subsample, ch] - ch, lw=1)
-            lines.append(line)
-        self.fig, self.axes, self.lines = fig, axes, lines
+        self.lines = [init_line(ch) for ch in range(n_chan)]
 
         self.refresh_step = int(0.2 / (12/data_buffer.sfreq))
 
+    def init_line(self, ch):
+        line, = self.axes.plot(self.data_buffer.times[::self.subsample],
+                               self.data_buffer.data[::self.subsample, ch] - ch,
+                               lw=1)
+        return line
+
+    def format_line_label(self, ch):
+        label = '{} - {}'.format(self.data_buffer.ch_names[ch],
+                                 self.impedances[ch])
+        return label
 
     def run(self):
         k = 0
