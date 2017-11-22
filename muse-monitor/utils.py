c67ead0b30d2541873ddf533d3b78835b03256a4
Matt Laporte <matt.l.laporte@gmail.com>
2017-11-22 12:14:11 -0500

    FFT and feature calculations.

--- muse-monitor/utils.py
+++ muse-monitor/utils.py
@@ -5,39 +5,81 @@
 
 import numpy as np
 
-def epoching_old(data, samples_epoch, samples_overlap=0):
-    n_samples, n_chan = data['ch_values'].shape
+BANDS = {
+    "theta": (4, 8),
+    "low_alpha": (8, 10),
+    "med_alpha": (9, 11),
+    "hi_alpha": (10, 12),
+    "low_beta": (12, 21),
+    "hi_beta": (21, 30),
+    "alpha": (8, 12),
+    "beta": (12, 30),
+}
+
+
+def epoching(samples, samples_epoch, samples_overlap=0):
+    n_samples, n_chan = samples.shape
 
     samples_shift = int(samples_epoch - samples_overlap)
-
     n_epochs = 1 + int(np.floor((n_samples - samples_epoch) / samples_shift))
+    epoched_samples = n_epochs * samples_epoch
 
-    markers = samples_shift * np.arange(0, n_epochs + 1)
-
+    samples_, remainder = np.split(samples, np.array([epoched_samples]))
+    markers = samples_shift * np.arange(n_epochs + 1)
     epochs = np.zeros((samples_epoch, n_chan, n_epochs))
-    for i_epoch in range(0,n_epochs):
-        epochs[:,:,i_epoch] = data['ch_values'][ markers[i_epoch] : markers[i_epoch] + samples_epoch ,:]
-
-    if (markers[-1] != n_samples):
-        remainder = data['ch_values'][markers[-1] : n_samples, :]
-    else:
-        remainder = np.asarray([])
+    for i_epoch in range(n_epochs):
+        ind_epoch = slice(markers[i_epoch], markers[i_epoch] + samples_epoch)
+        epochs[:, :, i_epoch] = samples[ind_epoch, :]
 
     return epochs , remainder
 
-def epoching(data, samples_epoch, samples_overlap=0):
+def epoching_alt(data, samples_epoch, samples_overlap=0):
     n_epochs = n_samples // samples_epoch
     epoched_samples = n_epochs * samples_epoch
+    data_, remainder = np.split(data, np.array([epoched_samples]))
 
-    epoch_rows = np.reshape(np.arange(0, epoched_samples),
-                            (samples_epoch, n_epochs)).T
-    rows = np.ravel(np.repeat(epoch_rows, n_chan, axis=0))
-    cols = np.tile(np.repeat(np.arange(0, n_chan), n_epochs), samples_epoch)
+    ind_rows = np.arange(epoched_samples).reshape(samples_epoch, n_epochs).T
+    ind_rows = np.ravel(np.repeat(ind_rows, n_chan, axis=0))
+    ind_cols = np.tile(np.repeat(np.arange(n_chan), n_epochs), samples_epoch)
 
-    epochs = np.reshape(data[:epoched_samples][rows, cols],
-                        (samples_epoch, n_chan, n_epochs))
-    try:
-        remainder = data[epoched_samples:]
-    except IndexError:
-        remainder = np.array([])
+    epochs = data_[ind_rows, ind_cols].reshape(samples_epoch, n_chan, n_epochs)
+
     return epochs, remainder
+
+def calc_fft(samples, sfreq, real_out=True):
+    n_samples, n_chan = samples.shape
+    hamming_window = np.hamming(n_samples)
+    samples_centered = samples - np.mean(samples, axis=0)
+    samples_centered_hamming = (samples_centered.T * hamming_window).T
+    n_fft = int(np.floor(np.log2(n_samples)))
+    fft = np.fft.fft(samples_centered_hamming, n=n_fft, axis=0) / n_samples
+    if real_out:
+        fft = fft[0:n_fft//2, :]
+    return fft
+
+def calc_psd(samples, sfreq):
+    fft = calc_fft(samples, sfreq)
+    psd = 2 * np.abs(fft)
+    return psd
+
+def calc_feature_vector(samples, sfreq, bands=BANDS):
+    psd = calc_psd(samples, sfreq)
+    f = sfreq/2 * np.linspace(0, 1, len(psd))
+    feature_vector = np.array([make_features(band, PSD, f)
+                               for band in bands.items()])
+    return feature_vector
+
+def make_features(band, psd, cutoff):
+	freqs = band[1] # get dict values, not key
+	ind = np.where((cutoff >= freqs[0]) & (cutoff <= freqs[1]))
+	return np.mean(psd[ind, :])
+
+def calc_feature_matrix(epochs, sfreq):
+    n_epochs = epochs.shape[2]
+    feat = calc_feature_vector(epochs[:,:,0], sfreq).T
+    feature_matrix = np.zeros((n_epochs, feat.shape[0]))
+
+    for i_epoch in range(0, n_epochs):
+        feature_matrix[i_epoch, :] = \
+            calc_feature_vector(epochs[:, :, i_epoch], sfreq).T
+    return feature_matrix
