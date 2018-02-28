e2d47781ff1bd57911b0fbd9f97f7630cf109b78
Matt L. Laporte <Matt.L.Laporte@gmail.com>
2018-02-28 16:48:04 -0500

    Moved data classes to utils and updated references. Removed experiment from monitor.py. Added TimeSeries file writing method.

--- monitor.py
+++ monitor.py
@@ -2,59 +2,10 @@
 # -*- coding: utf-8 -*-
 """Binary SVM classifier of concentrating vs. relaxating states from EEG data."""
 
-from musemonitor import classify, online, utils
-
-
-channels = ['TP9', 'AF7', 'AF8', 'TP10']
-
-
-# trial parameters
-lengths = [5, 5]  # seconds
-labels = [0, 1]
-messages = ["Please focus visually and concentrate.",
-            "Please close your eyes and relax."]
-
-# calculation parameters
-epoch_size = 100
+from musemonitor import online
+import gatt2lsl
 
 if __name__ == '__main__':
+    outlet_streamer = gatt2lsl.LSLOutletStreamer()
+
     streamer = online.LSLStreamer()
-    recorder = online.LSLRecorder(streamer)
-    sfreq = streamer.sfreq
-
-    # initial messages
-    welcome = """
-    EEG data will be recorded. During each trial, please be as still as \
-    possible while following each set of instructions.
-    """
-    print(welcome)
-
-    # record trials
-    trials = recorder.record_trials(lengths, messages)
-
-    # calculate epochs and features (EEG band means) for each trial
-    epoched = [utils.epoching(data['channels'], epoch_size) for data in trials]
-    feature_matrices = [utils.calc_feature_matrix(trial[0], sfreq)
-                        for trial in epoched]
-
-    classifier, mu_ft, std_ft = classify.train_binary_svm(feature_matrices,
-                                                          labels)
-
-    print("Press Enter to begin online classification.")
-    input()
-    streamer.proceed = False
-    streamer.join()
-
-    streamer2 = online.LSLStreamer(inlet=streamer.inlet, window=1)
-
-    streamer2.updated.clear()
-    while True:
-        streamer2.updated.wait()
-        data = streamer2.get_data()
-        streamer2.updated.clear()
-        epochs, remainder = utils.epoching(data['channels'],
-                                           streamer2.n_samples)
-        feature_matrix = utils.calc_feature_matrix(epochs, sfreq)
-        x = (feature_matrix - mu_ft) / std_ft
-        y_hat = classifier.predict(x)
-        print(y_hat)
--- musemonitor/online.py
+++ musemonitor/online.py
@@ -6,137 +6,11 @@ Management of data streams or other online processes.
 
 from musemonitor import utils
 
-import datetime
-import os
 from serial.serialutil import SerialException
 import threading
 
 import numpy as np
 import pylsl as lsl
-
-
-class Data:
-    """Parent of data management classes.
-    
-    Attributes:
-        metadata (dict): 
-    """
-
-    def __init__(self, metadata=None):     
-        
-        self.initialize()
-        
-        self.metadata = metadata
-
-        # thread control
-        self.updated = threading.Event()
-        self._lock = threading.Lock()
-
-    @property
-    def data(self):
-        """Return copy of data window."""
-        try:
-            with self._lock:
-                return np.copy(self._data)
-        except AttributeError:
-            raise NotImplementedError()
-
-    def initialize(self):
-        """Initialize data window."""
-        raise NotImplementedError()
-
-    def update(self):
-        """Update data."""
-        raise NotImplementedError()
-
-
-class TimeSeries(Data):
-    """
-    Attributes:
-        window (float): Number of seconds of most recent data to store.
-            Approximate, due to floor conversion to number of samples.
-    """
-
-    def __init__(self, ch_names, sfreq, window=10, record=True, metadata=None,
-                 filename=None, data_dir='data', label=None):
-        Data.__init__(self, metadata)
-        channels_dtype = np.dtype({'names': ch_names,
-                                   'formats': ['f8'] * len(ch_names)})
-        self._dtype = np.dtype([('time', 'f8'), ('channels', channels_dtype)])
-
-        self.sfreq = sfreq
-        self.window = window
-        self.n_samples = int(window * self.sfreq)
-        self._count = self.n_samples
-        if record:
-            if filename is None:
-                date = datetime.date.today().isoformat()
-                filename = './{}/timeseries_{}_{{}}.csv'.format(data_dir, date)
-                if label is None:
-                    # use next available integer label
-                    label = 0
-                    while os.path.exists(filename.format(label)):
-                        label += 1
-                filename = filename.format(label)
-
-            # make sure data directory exists
-            os.makedirs(filename[:filename.rindex(os.path.sep)], exist_ok=True)
-
-            self._file = open(filename, 'a')
-            
-
-    def initialize(self):
-        """Initialize stored samples to zeros."""
-        with self._lock:
-            self._data = np.zeros((self.n_samples,), dtype=self._dtype)
-            # self._data['time'] = np.arange(-self.window, 0, 1./self.sfreq)
-
-    def update(self, timestamps, samples):
-        """Append most recent chunk to stored data and retain window size."""
-        
-        new = self._format_samples(timestamps, samples)
-        
-        self._count -= len(new)
-        
-        # 
-        cutoff = len(new) + self._count
-        self._append(new[:cutoff])
-        if self._count < 1:
-            self._write_to_file()
-            self._count = self.n_samples
-        self._append(new[cutoff:])
-        
-        self.updated.set()
-        
-    def _append(self, new):
-        with self._lock:
-            self._data = np.concatenate([self._data, new], axis=0)
-            self._data = self.data[-self.n_samples:]
-        
-    def _write_to_file(self):
-        with self._lock:
-            np.savetxt(self._file, self._data)
-
-    def _format_samples(self, timestamps, samples):
-        """Format data `numpy.ndarray` from timestamps and samples."""
-        samples_tuples = [tuple(sample) for sample in samples]
-        data_array = np.array(list(zip(timestamps, samples_tuples)),
-                              dtype=self._dtype)
-        return data_array
-
-    @property
-    def ch_names(self):
-        """Names of channels."""
-        return self._data.dtype['channels'].names
-
-    @property
-    def window(self):
-        """Actual number of seconds stored.
-
-        Not necessarily the same as the requested window size due to flooring
-        to the nearest sample.
-        """
-        return self.n_samples / self.sfreq
 
 
 class LSLStreamer(threading.Thread):
@@ -187,7 +61,8 @@ class LSLStreamer(threading.Thread):
 
         # data class
         if data is None:
-            self.data = TimeSeries(self.ch_names, self.sfreq, metadata=None)
+            self.data = utils.TimeSeries(self.ch_names, self.sfreq,
+                                         metadata=None)
         else:
             self.data = data
 
