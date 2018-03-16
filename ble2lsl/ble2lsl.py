5323c8acd60e4db0210611723401ef7c2b5238af
Matt L. Laporte <Matt.L.Laporte@gmail.com>
2018-03-16 17:33:54 -0400

    Class and method comments for BLEStreamer (renamed from DeviceStreamer) and most of DummyStreamer. Several more simplifications in the inheritance relationship with OutletStreamer. Removed unnecessary arguments from DummyStreamer.impersonate_device. Made most of the attributes in BLEStreamer and DummyStreamer private, and turned `backend` into a property.

--- ble2lsl/ble2lsl.py
+++ ble2lsl/ble2lsl.py
@@ -11,7 +11,7 @@ Also includes dummy streamer objects, which do not acquire data over BLE but
 pass local data through an LSL outlet, e.g. for testing.
 
 TODO:
-    * Documentation for the two BLE backends.
+    * Documentation for both BLE backends.
 
 .. _Generic Attribute Profile:
    https://www.bluetooth.com/specifications/gatt/generic-attributes-overview
@@ -38,8 +38,8 @@ class OutletStreamer:
     Subclasses must implement `start` and `stop` methods for stream control.
 
     Attributes:
-        info (pylsl.StreamInfo):
-        outlet (pylsl.StreamOutlet):
+        info (pylsl.StreamInfo): Contains information about the stream.
+        outlet (pylsl.StreamOutlet): LSL outlet to which data is pushed.
 
     TODO:
         * get source_id... serial number from Muse? MAC address?
@@ -47,14 +47,14 @@ class OutletStreamer:
         * Some way to generalize autostart behaviour?
     """
 
-    def __init__(self, stream_params=None, chunk_size=None,
+    def __init__(self, stream_params=None, chunk_size=12,
                  time_func=time.time):
         """Construct an `OutletStreamer` object.
 
         Args:
-            stream_params (dict): Parameters to construct `pylsl.StreamInfo`
-            chunk_size (int): Number of samples pushed per LSL chunk
-            time_func (function): Function for generating timestamps
+            stream_params (dict): Parameters to construct `pylsl.StreamInfo`.
+            chunk_size (int): Number of samples pushed per LSL chunk.
+            time_func (function): Function for generating timestamps.
         """
         if stream_params is None:
             stream_params = {}
@@ -82,40 +82,62 @@ class OutletStreamer:
         self._data = np.zeros((self.info.channel_count(), self._chunk_size))
 
     def _push_chunk(self, channels, timestamps):
-        for sample in range(self.chunk_size):
+        for sample in range(self._chunk_size):
             pass
             #self.outlet.push_sample(channels[:, sample], timestamps[sample])
 
 
-class DeviceStreamer(OutletStreamer):
+class BLEStreamer(OutletStreamer):
+    """Streams data to an LSL outlet from a BLE device.
+
+    Attributes:
+        address (str): Device-specific (MAC) address.
+        start_time (float): Time of timestamp initialization by `initialize`.
+            Provided by `time_func`.
+
+    TODO:
+        * device-dependent bit indices in _transmit_packet
+        * device-dependent unit conversion in _unpack_channel
+        * move fake data generator to outside/iterator?
     """
 
-    """
+    def __init__(self, device_params, address=None, backend='bgapi',
+                 interface=None, autostart=True, **kwargs):
+        """Construct a `BLEStreamer` instance.
 
-    def __init__(self, device_params=None, stream_params=None, interface=None,
-                 address=None, backend='bgapi', autostart=True, chunk_size=12,
-                 **kwargs):
+        Args:
+            device_params (dict): Device-specific parameters.
+                Includes GATT handles and characteristics, channel UUIDs and
+                names, and other device metadata.
+            address (str): Device MAC address for establishing connection.
+                By default, this is acquired automatically.
+            backend (str): Which `pygatt` backend to use.
+                Allowed values are `'bgapi'` or `'gatt'`. The `'gatt'` backend
+                only works on Linux.
+            interface (str): The identifier for the BLE adapter interface.
+                When `backend='gatt'`, defaults to `'hci0'`.
+            autostart (bool): Whether to start streaming on instantiation.
         """
-
-        """
-        OutletStreamer.__init__(self, stream_params=stream_params,
-                                chunk_size=chunk_size, **kwargs)
-        self.device_params = device_params
-        self.interface = interface
+        OutletStreamer.__init__(self, **kwargs)
+        self._device_params = device_params
         self.address = address
 
         # initialize gatt adapter
-        if backend == 'gatt':
-            self.interface = self.interface or 'hci0'
-            self.adapter = pygatt.GATTToolBackend(self.interface)
-        elif backend == 'bgapi':
-            self.adapter = pygatt.BGAPIBackend(serial_port=self.interface)
+        if backend == 'bgapi':
+            self._adapter = pygatt.BGAPIBackend(serial_port=interface)
+        elif backend == 'gatt':
+            # only works on Linux
+            interface = self.interface or 'hci0'
+            self._adapter = pygatt.GATTToolBackend(interface)
         else:
             raise(ValueError("Invalid backend specified; use bgapi or gatt."))
-        self.backend = backend
+        self._backend = backend
 
         self._set_packet_format()
         self._add_device_info()
+        self._ble_params = self._device_params["ble"]
+
+        self.initialize_timestamping()
 
         if autostart:
             self.connect()
@@ -133,107 +155,89 @@ class DeviceStreamer(OutletStreamer):
                    stream_params=device.STREAM_PARAMS,
                    **kwargs)
 
-    def connect(self):
-        """
+    def initialize_timestamping(self):
+        """Reset the parameters for timestamp generation."""
+        self._sample_index = 0
+        self._last_tm = 0
+        self.start_time = self._time_func()
 
+    def start(self):
+        """Start streaming by writing to the relevant GATT characteristic."""
+        self._device.char_write_handle(self._ble_params["handle"],
+                                       value=self._ble_params["stream_on"],
+                                       wait_for_response=False)
+
+    def stop(self):
+        """Stop streaming by writing to the relevant GATT characteristic."""
+        self._device.char_write_handle(self._ble_params["handle"],
+                                       value=self._ble_params["stream_off"],
+                                       wait_for_response=False)
+
+    def disconnect(self):
+        """Disconnect from the BLE device and stop the adapter.
+
+        Note:
+            After disconnection, `start` will not resume streaming.
+
+        TODO:
+            * enable device reconnect with `connect`
         """
-        self.adapter.start()
+        self.stop()
+        self._device.disconnect()
+        self._adapter.stop()
+
+    def connect(self):
+        """Establish connection to BLE device (prior to `start`).
+
+        Starts the `pygatt` adapter, resolves the device address if necessary,
+        connects to the device, and subscribes to the channels specified in the
+        device parameters.
+        """
+        self._adapter.start()
         if self.address is None:
-            self.address = self._get_device_address(self.stream_params["name"])
+            # get the device address if none was provided
+            self.address = self._resolve_address(self._stream_params["name"])
         try:
-            self.device = self.adapter.connect(self.address)
+            # connect to the device
+            self._device = self._adapter.connect(self.address)
         except pygatt.exceptions.NotConnectedError:
             e_msg = "Unable to connect to device at address {}" \
                     .format(self.address)
             raise(IOError(e_msg))
 
-        for uuid in self.device_params["ch_uuids"]:
-            self.device.subscribe(uuid, callback=self._transmit_packet)
-
-    def start(self):
-        """
-
-        """
-        self.sample_index = 0
-        self.last_tm = 0
-        self.start_time = self.time_func()
-
-        ble_params = self.device_params["ble"]
-        self.device.char_write_handle(ble_params["handle"],
-                                      value=ble_params["stream_on"],
-                                      wait_for_response=False)
-
-    def stop(self):
-        """
-
-        """
-        ble_params = self.device_params["ble"]
-        self.device.char_write_handle(handle=ble_params["handle"],
-                                      value=ble_params["stream_off"],
-                                      wait_for_response=False)
-
-    def disconnect(self):
-        """
-
-        """
-        self.device.disconnect()
-        self.adapter.stop()
+        # subscribe to the channels specified by the device parameters
+        for uuid in self._device_params["ch_uuids"]:
+            self._device.subscribe(uuid, callback=self._transmit_packet)
 
     def _add_device_info(self):
+        """Adds device-specific parameters to `info`."""
         self.info.desc().append_child_value("manufacturer",
-                                            self.device_params["manufacturer"])
+                                            self._device_params["manufacturer"])
 
         self.channels = self.info.desc().append_child("channels")
-        for ch_name in self.device_params["ch_names"]:
+        for ch_name in self._device_params["ch_names"]:
             self.channels.append_child("channel") \
                 .append_child_value("label", ch_name) \
-                .append_child_value("unit", self.device_params["units"]) \
-                .append_child_value("type", self.stream_params["type"])
+                .append_child_value("unit", self._device_params["units"]) \
+                .append_child_value("type", self._stream_params["type"])
 
-    def _get_device_address(self, name):
-        list_devices = self.adapter.scan(timeout=10.5)
+    def _resolve_address(self, name):
+        list_devices = self._adapter.scan(timeout=10.5)
         for device in list_devices:
             if name in device['name']:
                 return device['address']
         raise(ValueError("No devices found with name `{}`".format(name)))
 
     def _set_packet_format(self):
-        dtypes = self.device_params["packet_dtypes"]
-        self.packet_format = dtypes["index"] + \
-                             (',' + dtypes["ch_value"]) * self.chunk_size
-
-    def _transmit_packet(self, handle, data):
-        """TODO: Move bit locations to Muse parameters."""
-        timestamp = self.time_func()
-        index = int((handle - 32) / 3)
-
-        tm, d = self._unpack_channel(data)
-
-        if self.last_tm == 0:
-            self.last_tm = tm - 1
-
-        self.data[index] = d
-        self.timestamps[index] = timestamp
-
-        # if last channel in chunk
-        if handle == 35:
-            if tm != self.last_tm + 1:
-                print("Missing sample {} : {}".format(tm, self.last_tm))
-            self.last_tm = tm
-
-            # sample indices
-            sample_indices = np.arange(self.chunk_size) + self.sample_index
-            self.sample_index += self.chunk_size
-
-            timestamps = sample_indices / self.info.nominal_srate() \
-                         + self.start_time
-
-            self._push_chunk(self.data, timestamps)
-            self._init_sample()
+        """Defines the format for parsing bit strings received over BLE."""
+        dtypes = self._device_params["packet_dtypes"]
+        self._packet_format = dtypes["index"] + \
+                              (',' + dtypes["ch_value"]) * self._chunk_size
 
     def _unpack_channel(self, packet):
+        """Parse the bitstrings received over BLE."""
         packet_bits = bitstring.Bits(bytes=packet)
-        unpacked = packet_bits.unpack(self.packet_format)
+        unpacked = packet_bits.unpack(self._packet_format)
 
         packet_index = unpacked[0]
         packet_values = np.array(unpacked[1:])
@@ -241,15 +245,58 @@ class DeviceStreamer(OutletStreamer):
 
         return packet_index, packet_values
 
+    def _transmit_packet(self, handle, data):
+        """Callback function used by `pygatt` to receive BLE data."""
+        timestamp = self._time_func()
+        index = int((handle - 32) / 3)
+
+        tm, d = self._unpack_channel(data)
+
+        if self._last_tm == 0:
+            self._last_tm = tm - 1
+
+        self._data[index] = d
+        self._timestamps[index] = timestamp
+
+        # if last channel in chunk
+        if handle == 35:
+            if tm != self._last_tm + 1:
+                print("Missing sample {} : {}".format(tm, self._last_tm))
+            self._last_tm = tm
+
+            # sample indices
+            sample_indices = np.arange(self._chunk_size) + self._sample_index
+            self._sample_index += self._chunk_size
+
+            timestamps = sample_indices / self.info.nominal_srate() \
+                         + self.start_time
+
+            self._push_chunk(self._data, timestamps)
+            self._init_sample()
+
+    @property
+    def backend(self):
+        """The `pygatt` backend used by the instance."""
+        return self._backend
+
 
 class DummyStreamer(OutletStreamer):
-    """
+    """Streams data over an LSL outlet from a local source.
 
+    Attributes:
+        sfreq (int): Sampling frequency for generated data.
+        n_chan (int): Number of channels of generated data.
+        csv_file (str): Filename of `.csv` containing local data.
+
+    TODO:
+        * take a data iterator instead of multiple optional parameters
     """
 
     def __init__(self, dummy_func=None, dur=60, sfreq=256, n_chan=4,
                  csv_file=None, autostart=True, **kwargs):
-        """
+        """Construct a `DummyStreamer` instance.
+
+        Attributes:
 
         """
         OutletStreamer.__init__(self, **kwargs)
@@ -260,7 +307,7 @@ class DummyStreamer(OutletStreamer):
         if csv_file is None:
             self.sfreq = sfreq
             self.n_chan = n_chan
-            self.fake_data = self.gen_fake_data(dur)
+            self._dummy_data = self.gen_fake_data(dur)
         else:
             self.csv_file = csv_file
             # TODO:load csv file to np array
@@ -270,15 +317,13 @@ class DummyStreamer(OutletStreamer):
             self._thread.start()
 
     @classmethod
-    def impersonate_device(cls, device, dummy_func=None, dur=60, csv_file=None,
-                           **kwargs):
-        """
+    def impersonate_device(cls, device, **kwargs):
+        """Construct a `DummyStreamer` instance to mimic a device's metadata.
 
         Args:
-
+            device: A device module from `ble2lsl.devices`.
         """
-        return cls(dur=dur,
-                   sfreq=device.STREAM_PARAMS['nominal_srate'],
+        return cls(sfreq=device.STREAM_PARAMS['nominal_srate'],
                    n_chan=device.STREAM_PARAMS['channel_count'],
                    device_params=device.PARAMS,
                    stream_params=device.STREAM_PARAMS,
@@ -287,31 +332,41 @@ class DummyStreamer(OutletStreamer):
     def _stream(self):
         while self._proceed:
             self.start_time = self.time_func()
-            chunk_inds = np.arange(0, len(self.fake_data.T), self.chunk_size)
+            chunk_inds = np.arange(0, len(self._dummy_data.T), self._chunk_size)
             for chunk_ind in chunk_inds:
                 self.make_chunk(chunk_ind)
-                self._push_chunk(self.data, self.timestamps)
+                self._push_chunk(self._data, self._timestamps)
                 # force sampling rate
-                sec_per_chunk = 1/(self.srate/self.chunk_size)
+                sec_per_chunk = 1 / (self.sfreq / self._chunk_size)
                 time.sleep(sec_per_chunk)
 
-    def gen_fake_data(self, dur, freqs=[5, 10, 12, 20]):
-        n_fake_samples = dur*self.srate
-        x = np.arange(0, n_fake_samples)
-        a_freqs = 2*np.pi*np.array(freqs)
+    def gen_dummy_data(self, dur, freqs=[5, 10, 12, 20]):
+        """Generate noisy sinusoidal dummy samples.
+
+        TODO:
+            * becomes external when passing an iterator to `DummyStreamer`
+        """
+        n_samples = dur * self.srate
+        x = np.arange(0, n_samples)
+        a_freqs = 2 * np.pi * np.array(freqs)
         y = np.zeros((self.n_chan, len(x)))
 
         # sum frequencies with random amplitudes
         for freq in a_freqs:
-            y += np.random.randint(1,5)*np.sin(freq*x)
+            y += np.random.randint(1,5) * np.sin(freq * x)
 
-        noise = np.random.normal(0, 1, (self.n_chan, n_fake_samples))
-        fake_data = y + noise
+        noise = np.random.normal(0, 1, (self.n_chan, n_samples))
+        dummy_data = y + noise
 
-        return fake_data
+        return dummy_data
 
     def make_chunk(self, chunk_ind):
-        self.data = self.fake_data[:,chunk_ind:chunk_ind+self.chunk_size]
+        """Prepare a chunk from the totality of local data.
+
+        TODO:
+            * replaced when using an iterator
+        """
+        self._data = self._dummy_data[:,chunk_ind:chunk_ind+self.chunk_size]
         # TODO: more realistic timestamps
         timestamp = self.time_func()
-        self.timestamps = np.array([timestamp]*self.chunk_size)
+        self._timestamps = np.array([timestamp]*self.chunk_size)
