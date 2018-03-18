Fixed array length retention in utils.push_rows. In acquire, temporary fixes for `data` module masking and StreamInlet dropping of channel names in LSLStreamer __init__, as well as switching to the new TimeSeries.with_window. In data.TimeSeries, added self._record which was forgotten in earlier edit, property unstructured which returns a normal np.ndarray of the channel data, fixed treatment of n_samples in initialize, and added property n_chan to prevent repeated calling of len(ch_names) elsewhere. Improved class structure of plot.py.

wizardhat/acquire.py
@@ -18,7 +18,7 @@ Note:
    https://github.com/sccn/labstreaminglayer
 """
 
-from wizardhat import utils, data
+from wizardhat import data
 
 from serial.serialutil import SerialException
 import threading
@@ -40,7 +40,7 @@ class LSLStreamer:
 
     """
 
-    def __init__(self, inlet=None, data=None, dejitter=True,
+    def __init__(self, inlet=None, data_=None, dejitter=True,
                  chunk_samples=12, autostart=True):
         """Instantiate LSLStreamer given length of data store in seconds.
 
@@ -71,16 +71,19 @@ class LSLStreamer:
         info = inlet.info()
         self.sfreq = info.nominal_srate()
         self.n_chan = info.channel_count()
-        self.ch_names = get_ch_names(info)
+        # self.ch_names = get_ch_names(info)
+        from ble2lsl.devices import muse
+        self.ch_names = list(muse.PARAMS['ch_names'])
+        #print(self.ch_names)
 
         # instantiate the `data.TimeSeries` instance if one is not provided
-        if data is None:
+        if data_ is None:
             metadata = {"pipeline": [type(self).__name__]}
-            self.data = data.TimeSeries(self.ch_names, self.sfreq,
-                                        metadata=metadata)
+            self.data = data.TimeSeries.with_window(self.ch_names, self.sfreq,
+                                                    metadata=metadata)
         else:
             # user-defined instance
-            self.data = data
+            self.data = data_
             # TODO: do a test update to make sure it's a TimeSeries instance
             #try:
             #    test_samples = np.zeros(0, dtype=self.data.dtype)

wizardhat/data/data.py
@@ -116,6 +116,16 @@ class Data:
             raise NotImplementedError()
 
     @property
+    def unstructured(self):
+        """Return structured as regular `np.ndarray`.
+
+        TODO:
+            * dtype (np.float64?) based on context
+            * ValueError if self._data is not structured?
+        """
+        return self.data.view((np.float64, self.n_chan + 1))
+
+    @property
     def dtype(self):
         """Data type specification for the instance."""
         return self._dtype
@@ -143,7 +153,6 @@ class Data:
         """
         self.metadata['pipeline'].append(type(obj).__name__)
         self._write_metadata_to_file()
-
 
     def _write_metadata_to_file(self):
         try:
@@ -218,7 +227,9 @@ class TimeSeries(Data):
         except ValueError:
             raise ValueError("Number of formats must match number of channels")
 
-        self.initialize(n_samples)
+        self._record = record
+
+        self.initialize(int(n_samples))
 
     @classmethod
     def with_window(cls, ch_names, sfreq, window=10, **kwargs):
@@ -242,15 +253,17 @@ class TimeSeries(Data):
         n_samples = int(window * sfreq)
         return cls(ch_names, n_samples, **kwargs)
 
-    def initialize(self, n_samples):
+    def initialize(self, n_samples=None):
         """Initialize NumPy structured array for data storage.
 
         Args:
             n_samples (int): Number of samples (rows) in array.
         """
+        if n_samples is None:
+            n_samples = self.n_samples
         with self._lock:
-            self._data = np.zeros((self.n_samples,), dtype=self._dtype)
-        self._count = self.n_samples
+            self._data = np.zeros((n_samples,), dtype=self._dtype)
+        self._count = n_samples
 
     def update(self, timestamps, samples):
         """Append sample(s) to stored data.
@@ -267,7 +280,7 @@ class TimeSeries(Data):
         cutoff = len(new) + self._count
         self._append(new[:cutoff])
         if self._count < 1:
-            if self.record:
+            if self._record:
                 self._write_to_file()
             self._append(new[cutoff:])
             self._count = self.n_samples
@@ -303,6 +316,11 @@ class TimeSeries(Data):
             Does not include `'time'`.
         """
         return self.dtype.names[1:]
+
+    @property
+    def n_chan(self):
+        """Number of channels."""
+        return len(self.ch_names)
 
     @property
     def samples(self):

wizardhat/plot/plot.py
@@ -13,64 +13,70 @@ from vispy import gloo, app, visuals
 
 class Plotter(app.Canvas):
     """ """
-    def __init__(self, data_in, title='Plotter'):
-        app.Canvas.__init__(self, title=title, keys='interactive')
-        self.data_in = data_in
-        # self.data = np.array([data[self.ch_names[0]],data[self.ch_names[1]], data[self.ch_names[2]], data[self.ch_names[3]], data[self.ch_names[4]]])
-        # self.data = self.data.T
+    def __init__(self, data, plot_params=None):
+        app.Canvas.__init__(self, keys='interactive')
+
+        self.data = data
 
 
-class Plotter2D(Plotter):
-    def __init__(self, data, signal, scale=500):
+class Lines(Plotter):
+    def __init__(self, data, plot_params=None, **kwargs):
+        super().__init__(data, plot_params=plot_params, **kwargs)
 
-        self.signal = signal  # either 'raw' or 'psd'
+        self.n_lines = self.data.n_chan
+        self.n_points = self.data.n_samples
 
-        # Number of cols and rows in the table.
-        nrows = len(data.ch_names)
-        ncols = 1
+        if plot_params is None:
+            plot_params = {}
+        try:
+            self.params = dict(
+                title='Plotter',
+                font_size=48,
+                scale=500,
+                palette=color_palette("RdBu_r", self.n_lines),
+                quality_palette=color_palette("RdYlGn", 11)[::-1],
+            )
+            self.params.update(plot_params)
+        except TypeError:
+            raise TypeError("plot_params not a dict or key-value pairs list")
 
-        m = nrows * ncols  # Number of signals.
-        n = data.n_samples # Number of samples per signal.
+        color = np.repeat(self.params['palette'],
+                          self.n_points, axis=0).astype(np.float32)
 
-        # Various signal amplitudes.
-        amplitudes = np.zeros((m, n)).astype(np.float32)
-        gamma = np.ones((m, n)).astype(np.float32)
+        self._set_program_params(color)
 
-        # Generate the signals as a (m, n) array.
-        y = amplitudes
+        # text
+        self._names = [visuals.TextVisual(ch_name, bold=True, color='white')
+                       for ch_name in self.data.ch_names]
+        self._quality = [visuals.TextVisual('', bold=True, color='white')
+                         for ch_name in self.data.ch_names]
 
-        color = color_palette("RdBu_r", nrows)
-        color = np.repeat(color, n, axis=0).astype(np.float32)
+        self._init_plot()
 
-        # Signal 2D index of each vertex (row and col) and x-index (sample index
-        # within each signal).
-        index = np.c_[np.repeat(np.repeat(np.arange(ncols), nrows), n),
-                      np.repeat(np.tile(np.arange(nrows), ncols), n),
-                      np.tile(np.arange(n), m)].astype(np.float32)
+    def _set_program_params(self, color):
+
+        # Signal 2D index of each vertex (row and col) and x-index (sample
+        # index within each signal).
+        index = np.c_[np.repeat(np.repeat(np.arange(1), self.n_lines),
+                                self.n_points),
+                      np.repeat(np.tile(np.arange(self.n_lines), 1),
+                                self.n_points),
+                      np.tile(np.arange(self.n_points),
+                              self.n_lines)].astype(np.float32)
+
+        position = np.zeros((self.n_lines,
+                             self.n_points)).astype(np.float32).reshape(-1, 1)
 
         self.program = gloo.Program(shaders.VERT_SHADER, shaders.FRAG_SHADER)
-        self.program['a_position'] = y.reshape(-1, 1)
+        self.program['a_position'] = position
         self.program['a_color'] = color
         self.program['a_index'] = index
         self.program['u_scale'] = (1., 1.)
-        self.program['u_size'] = (nrows, ncols)
-        self.program['u_n'] = n
+        self.program['u_size'] = (self.n_lines, 1)
+        self.program['u_n'] = self.n_points
 
-        # text
-        self.font_size = 48.
-        self.names = []
-        self.quality = []
-        for ii in range(self.n_chan):
-            text = visuals.TextVisual(self.ch_names[ii], bold=True, color='white')
-            self.names.append(text)
-            text = visuals.TextVisual('', bold=True, color='white')
-            self.quality.append(text)
-
-        self.quality_colors = color_palette("RdYlGn", 11)[::-1]
-
-        self.scale = scale
-
-        self._timer = app.Timer('auto', connect=self.updatePlot, start=True)
+    def _init_plot(self):
+        self._timer = app.Timer('auto', connect=self.update_plot, start=True)
         gloo.set_viewport(0, 0, *self.physical_size)
         gloo.set_state(clear_color='black', blend=True,
                        blend_func=('src_alpha', 'one_minus_src_alpha'))
@@ -98,24 +104,24 @@ class Plotter2D(Plotter):
         self.program['u_scale'] = (max(1, scale_x_new), max(0.01, scale_y_new))
         self.update()
 
-    def updatePlot(self, data):
-        if self.signal == 'raw':
-            plot_data = (self.data - self.data.mean(axis=0)) / self.scale
-        elif self.signal =='psd':
-            #plot_data = Transformer.FFT(self.data) #Uncomment when transformer class is ready
-            self.signal == 'raw'
-            pass
-        sd = np.std(plot_data[-int(self.sfreq):], axis=0)[::-1] * self.scale
+    def update_plot(self, data):
+        plot_data = self.data.unstructured[:, 1:]
+        plot_data = (plot_data - plot_data.mean(axis=0)) / self.params['scale']
+        sd = np.std(plot_data[-int(256):], axis=0)[::-1]
+        #sd = np.std(plot_data[-int(self.data.sfreq):], axis=0)[::-1]
+        sd *= self.params['scale']
         co = np.int32(np.tanh((sd - 30) / 15)*5 + 5)
-        for ii in range(self.n_chan):
-            self.quality[ii].text = '%.2f' % (sd[ii])
-            self.quality[ii].color = self.quality_colors[co[ii]]
-            self.quality[ii].font_size = 12 + co[ii]
 
-            self.names[ii].font_size = 12 + co[ii]
-            self.names[ii].color = self.quality_colors[co[ii]]
+        for l in range(self.n_lines):
+            self._quality[l].text = '%.2f' % (sd[l])
+            self._quality[l].color = self.params['quality_palette'][co[l]]
+            self._quality[l].font_size = 12 + co[l]
 
-        self.program['a_position'].set_data(plot_data.T.ravel().astype(np.float32))
+            self._names[l].font_size = 12 + co[l]
+            self._names[l].color = self.params['quality_palette'][co[l]]
+
+        plot_data = plot_data.T.ravel().astype(np.float32)
+        self.program['a_position'].set_data(plot_data)
         self.update()
 
     def on_resize(self, event):
@@ -123,65 +129,19 @@ class Plotter2D(Plotter):
         vp = (0, 0, self.physical_size[0], self.physical_size[1])
         self.context.set_viewport(*vp)
 
-        for ii, t in enumerate(self.names):
+        for l, t in enumerate(self._names):
             t.transforms.configure(canvas=self, viewport=vp)
-            t.pos = (self.size[0] * 0.025, ((ii + 0.5)/self.n_chan) * self.size[1])
+            t.pos = (self.size[0] * 0.025,
+                     ((l + 0.5)/self.n_lines) * self.size[1])
 
-        for ii, t in enumerate(self.quality):
+        for l, t in enumerate(self._quality):
             t.transforms.configure(canvas=self, viewport=vp)
-            t.pos = (self.size[0] * 0.975, ((ii + 0.5)/self.n_chan) * self.size[1])
+            t.pos = (self.size[0] * 0.975,
+                     ((l + 0.5)/self.n_lines) * self.size[1])
 
     def on_draw(self, event):
         gloo.clear()
         gloo.set_viewport(0, 0, *self.physical_size)
         self.program.draw('line_strip')
-        [t.draw() for t in self.names + self.quality]
-
-
-#class signalPlotter(Plotter)
- #   def __init__(self)
-  #  self.initialize()
-#
- #   def initialize(self):
-        #streams, figures, assign colours, choose scale
-        #prepare to take samples, know how many samples dealing with
-        #draw channel names
-  #      pass
-
-   # def _draw(self):
-        #Redraw figures with new updated information from TimeSeries
-        #show raw voltage values on graph
-    #    pass
-
-    #def _on_filter_click(self, event):
-        #redirect data to a filter function
-        #call draw function with new filtered data
-     #   pass
-
-    #def _on_mouse_wheel(self, event):
-        #rescale data (change window size)
-     #   pass
-
-    #def _mark_data(self, event):
-        #manual add of event marker to be saved for later?
-        #must call data save function to add the marker to appropriate timestamp
-     #   pass
-
-#class psdPlotter(Plotter)
- #   def __init__(self,data)
-  #  self.initialize():
-   # pass
-    #def initialize():
-        #set up view of power spectrum streaming
-        #need to know how many bands to show
-        #set up sample recieving variables
-     #   pass
-
-   # def _draw():
-        #redraw PSD spectrum when new samples come in
-
-    #    pass
-
-    #def _process_sample():
-        #call sample preprocessing function to generate frequency domain data
-     #   pass
+        for t in self._names + self._quality:
+            t.draw()

wizardhat/utils.py
@@ -51,8 +51,9 @@ def push_rows(arr, rows):
         arr (Iterable): Array onto which to append the rows.
         rows (Iterable): Rows to be appended.
     """
+    n = arr.shape[0]
     arr = np.concatenate([arr, rows], axis=0)
-    return arr[-arr.shape[0]:]
+    return arr[-n:]
 
 
 def makedirs(filepath):