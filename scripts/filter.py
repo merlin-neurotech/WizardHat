cdba3ce97b43de4b654f1b8a05e00a97507b4c7d
Matt L. Laporte <Matt.L.Laporte@gmail.com>
2018-03-18 22:15:45 -0400

    Final version of the basic filter and dummy scripts.

--- scripts/filter.py
+++ scripts/filter.py
@@ -1,17 +1,15 @@
 
-"""
+"""Basic EEG filtering example using MNE."""
 
-"""
-
-#import ble2lsl
-#from ble2lsl.devices import muse2016
+import ble2lsl
+from ble2lsl.devices import muse2016
 from wizardhat import acquire, transform, plot
 
 from vispy import app
 
 if __name__ == '__main__':
     # connect to Muse headset and stream samples through LSL outlet
-    #outlet_streamer = ble2lsl.BLEStreamer.from_device(muse2016)
+    outlet_streamer = ble2lsl.BLEStreamer.from_device(muse2016)
     
     # automatically resolve and connect to LSL outlet and store samples in an
     # instance of wizardhat.data.TimeSeries (the attribute streamer.data),
