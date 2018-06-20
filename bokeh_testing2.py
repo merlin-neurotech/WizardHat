ced217a198e1f3dace4eed97f0528ce07a3bfc0a
Matt L. Laporte <Matt.L.Laporte@gmail.com>
2018-06-20 15:33:49 -0400

    Simplified set_layout and added color palette parameter and y axis labels (channel names). Generalized so that `ColumnDataSource` is built from dtype of `Data` object. Moved script elements to __name__ == '__main__'. Consolidated some instance methods and made some private. Small corrections to formatting/standardization.

--- bokeh_testing2.py
+++ bokeh_testing2.py
@@ -1,25 +1,13 @@
-import ble2lsl
-from ble2lsl.devices import muse2016
-from wizardhat import acquire
-import math
-import numpy as np
-from bokeh.plotting import figure, output_file, show, curdoc
-from bokeh.models import ColumnDataSource
-from bokeh.transform import jitter
-from bokeh.layouts import row, column, gridplot
-from bokeh.driving import count
-from bokeh.server.server import Server
-
-"""Rough implementation of a standalone bokeh server. 
+"""Rough implementation of a standalone bokeh server.
 
 bokeh serve --show bokeh_server.py    <-- to run
 
-Currently just grabs the most recent sample from Data.data every time the 
-periodic callback executes. This is probably not the best way to do it, because 
-the sampling rate is arbitrarily based on the value for add_periodic_callback(). 
-For example, you can set the callback time to something faster than the sampling 
-rate and you'll see that each value in streamer.data.data gets sampled a few 
-times (starts to look like a step function). Right now there's no good way to 
+Currently just grabs the most recent sample from Data.data every time the
+periodic callback executes. This is probably not the best way to do it, because
+the sampling rate is arbitrarily based on the value for add_periodic_callback().
+For example, you can set the callback time to something faster than the sampling
+rate and you'll see that each value in streamer.data.data gets sampled a few
+times (starts to look like a step function). Right now there's no good way to
 check that we're not dropping samples when updating.
 
 Also just two manually retrieved channels for now as a proof of concept, but
@@ -32,8 +20,21 @@ TODO:
     * Automatically determine device name/set to title?
     *Automatically determine source structure without explicit dictionary call
 """
-dummy_outlet = ble2lsl.DummyStreamer(muse2016)
-streamer = acquire.LSLStreamer()
+
+import ble2lsl
+from ble2lsl.devices import muse2016
+from wizardhat import acquire
+
+import math
+from bokeh.driving import count
+from bokeh.layouts import row, column, gridplot
+from bokeh.models import ColumnDataSource
+from bokeh.palettes import all_palettes as palettes
+from bokeh.plotting import figure, output_file, show, curdoc
+from bokeh.server.server import Server
+from bokeh.transform import jitter
+
+import numpy as np
 
 
 class Lines():
@@ -44,7 +45,7 @@ class Lines():
     form (number of channels and rows/samples); the user can cycle between
     plots of each data source with the 'D' key.
     """
-    def __init__(self, data):
+    def __init__(self, data, palette='Category10', autostart=True):
         """Construct a `Lines` instance.
         Args:
             data (data.Data or List[data.Data]): Data object(s) managing data
@@ -56,87 +57,53 @@ class Lines():
             plot_params (dict): Plot display parameters.
         """
 
-        try:
-            self.ch_names
-            self.data = [self.data]
-        except AttributeError:
-            pass
-        
         self.data = data
-        self.source = ColumnDataSource({'time':[], 'TP9':[], 'AF7':[],'AF8':[],'TP10':[]})#,'Right AUX':[]})
-        self.start()
-    def start(self):
-        self.run_server()
-        #self.set_layout()
-        #self.set_callbacks()
-    
-    def set_layout(self):
-        self.p = figure(plot_height=100, tools="xpan,xwheel_zoom,xbox_zoom,reset", 
-        x_axis_type=None, y_axis_location="right")
-        self.p.x_range.follow = "end" # always follows new data in source
-        self.p.x_range.follow_interval = 5 # in s (based on the actual value of the x in p.line)
-        self.p.x_range.range_padding = 0 # we can play with this stuff
+        data_dict = {name: [] for name in self.data.dtype.names}
+        self.source = ColumnDataSource(data_dict)
 
-        self.p2 = figure(plot_height=100, tools="xpan,xwheel_zoom,xbox_zoom,reset", 
-        x_axis_type=None, y_axis_location="right")
-        self.p2.x_range.follow = "end" # always follows new data in source
-        self.p2.x_range.follow_interval = 5 # in s (based on the actual value of the x in p.line)
-        self.p2.x_range.range_padding = 0 # we can play with this stuff
+        self._colors = palettes[palette][len(self.data.ch_names)]
 
-        self.p3 = figure(plot_height=100, tools="xpan,xwheel_zoom,xbox_zoom,reset", 
-        x_axis_type=None, y_axis_location="right")
-        self.p3.x_range.follow = "end" # always follows new data in source
-        self.p3.x_range.follow_interval = 5 # in s (based on the actual value of the x in p.line)
-        self.p3.x_range.range_padding = 0 # we can play with this stuff
+        if autostart:
+            self.run_server()
 
-        self.p4 = figure(plot_height=100, tools="xpan,xwheel_zoom,xbox_zoom,reset", 
-        x_axis_type=None, y_axis_location="right")
-        self.p4.x_range.follow = "end" # always follows new data in source
-        self.p4.x_range.follow_interval = 5 # in s (based on the actual value of the x in p.line)
-        self.p4.x_range.range_padding = 0 # we can play with this stuff
-        
-        self.p.line(x='time', y='TP9', alpha=0.8, line_width=2, color='blue', source=self.source)
-        self.p2.line(x='time', y='AF7', alpha=0.8, line_width=2, color='blue', source=self.source)
-        self.p3.line(x='time', y='AF8', alpha=0.8, line_width=2, color='blue', source=self.source)
-        self.p4.line(x='time', y='TP10', alpha=0.8, line_width=2, color='blue', source=self.source)
-          
-    
-    def _reformat_new_sample(self,new_sample):
-        data_dict = {'time':[new_sample['time']]}
-        for chan in self.data.ch_names[0:4]: #Exclude Aux
-            data_dict[chan] = [(new_sample[chan])]
-
-        return data_dict
+    def _set_layout(self):
+        self.p = []
+        for i, ch in enumerate(self.data.ch_names):
+            p = figure(plot_height=100,
+                       tools="xpan,xwheel_zoom,xbox_zoom,reset",
+                       x_axis_type=None, y_axis_location="right")
+            p.x_range.follow = "end"  # always follows new data in source
+            p.x_range.follow_interval = 5  # in s (based on the actual value of the x in p.line)
+            p.x_range.range_padding = 0  # we can play with this stuff
+            p.yaxis.axis_label = ch
+            p.line(x='time', y=ch, alpha=0.8, line_width=2,
+                   color=self._colors[i], source=self.source)
+            self.p.append([p])
 
     def update(self):
         new_sample = self.data.data[-1]
-        data_dict = self._reformat_new_sample(new_sample)
-        #self.source = ColumnDataSource(data_dict) 
+        data_dict = {ch: [new_sample[ch]] for ch in self.data.dtype.names}
         self.source.stream(data_dict, 500)
-    
-    #def draw(self):
-    #    y_range = self.data.ch_names
-    #    p = figure(plot_width=800,y_range=y_range,plot_height=600, x_axis_type='datetime')
-        
-    #    for chan in self.data.ch_names:
-    #        p.line(x='time',y=chan,source=self.source)
-            
-    #    show(p)
-    
-    def set_callbacks(self):
-        self.curdoc.add_root(gridplot([[self.p],[self.p2],[self.p3],[self.p4]], toolbar_location="left", plot_width=1000))
+
+    def _set_callbacks(self):
+        self.curdoc.add_root(gridplot(self.p, toolbar_location="left",
+                                      plot_width=1000))
         self.curdoc.add_periodic_callback(self.update, 50) # in ms
         self.curdoc.title = "Dummy EEG Stream"
-    
-    def app_manager(self,curdoc):
-        self.curdoc=curdoc
-        self.set_layout()
-        self.set_callbacks()
-    
+
+    def _app_manager(self, curdoc):
+        self.curdoc = curdoc
+        self._set_layout()
+        self._set_callbacks()
+
     def run_server(self):
-        server = Server({'/':self.app_manager})
+        server = Server({'/': self._app_manager})
         server.start()
-        server.io_loop.add_callback(server.show,"/")
+        server.io_loop.add_callback(server.show, '/')
         server.io_loop.start()
 
-Lines(streamer.data)
+
+if __name__ == '__main__':
+    dummy_outlet = ble2lsl.DummyStreamer(muse2016)
+    streamer = acquire.LSLStreamer()
+    Lines(streamer.data)
