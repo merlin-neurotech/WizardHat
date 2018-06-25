cfa1b62b3d88f87445a057023bbe0f4f2f7d020d
Matt L. Laporte <Matt.L.Laporte@gmail.com>
2018-06-25 16:13:27 -0400

    Subscribing to receive characteristic and start streaming, simple printing.

--- testing_openbci.py
+++ testing_openbci.py
@@ -2,13 +2,16 @@ import pygatt
 from pygatt.backends import BLEAddressType
 import binascii
 
+def print_packet(handle, data):
+    print(handle, data, '\n')
+
 name = 'Ganglion'
 adapter = pygatt.BGAPIBackend()
 adapter.start()
 devices = adapter.scan()
 
 ble = dict(service='fe84',
-recieve='2d30c082f39f4ce6923f3484ea480596',
+receive='2d30c082f39f4ce6923f3484ea480596',
 send="2d30c083f39f4ce6923f3484ea480596",
 disconnect="2d30c084f39f4ce6923f3484ea480596")
 
@@ -18,5 +21,7 @@ for device in devices:
 
 device = adapter.connect(address, address_type=BLEAddressType.random)
 
-device.char_write_handle(ble['service'], value=ble['recieve'],wait_for_response=False)
-print('Streaming')
+device.subscribe(ble['receive'], callback=print_packet)
+device.char_write(ble['send'], value=b'b', wait_for_response=False)
+
+
